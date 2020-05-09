11482f520be8feac506068e7b26ded010a22c4aa
Djordje <miladinovic.djordje@gmail.com>
2020-05-09 20:06:35 +0200

    Developed select by id, insert, update and delete

--- src/herbarium/core.clj
+++ src/herbarium/core.clj
@@ -3,3 +3,13 @@
 )
 
 (println (db/get-herbs))
+
+(db/insert-herb "testName" 1 100 0)
+
+(db/update-herb 7 "testNameUpdate" 3 300 1)
+
+(db/delete-herb 8)
+
+(println (db/get-herbs))
+
+(println (db/get-herb 1))
--- src/herbarium/database.clj
+++ src/herbarium/database.clj
@@ -11,6 +11,40 @@
 
 (defn get-herbs []
   (jdbc/query db-con
-             ["SELECT * FROM herbs"]
+              ["SELECT * FROM herbs "]
+  )
+)
+
+(defn get-herb [id]
+  (jdbc/query db-con
+              ["SELECT * FROM herbs WHERE id = ?" id]
+  )
+)
+
+(defn insert-herb [n t gh e]
+  (jdbc/insert! db-con
+                :herbs {:name n
+                        :type t
+                        :growheight gh
+                        :eatable e
+                        }
+  )
+)
+
+(defn update-herb [id n t gh e]
+  (jdbc/update! db-con
+                :herbs {:name n
+                        :type t
+                        :growheight gh
+                        :eatable e
+                        }
+                ["id = ?" id]
+  )
+)
+
+(defn delete-herb [id]
+  (jdbc/delete! db-con
+                :herbs
+                ["id = ?" id]
   )
 )
